a7d20ad4cbdf55d1294ca6f71cee3cf63385c358
Andres Parra <byandrev@gmail.com>
2023-07-06 10:01:35 -0500

    init

---
+++ src/main/java/com/byandrev/lnks/LnksApplication.java
@@ -0,0 +1,13 @@
+package com.byandrev.lnks;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LnksApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LnksApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/byandrev/lnks/LnksApplicationTests.java
@@ -0,0 +1,13 @@
+package com.byandrev.lnks;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LnksApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
